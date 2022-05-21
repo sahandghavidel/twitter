09dd9e4b10af6fe50ddbb5a79e29322e2a36a3a3
Sahandgha <sahand.gha@gmail.com>
2022-05-21 11:34:28 +1000

    add delete functionality to the post updated for image delete error

--- components/Post.js
+++ components/Post.js
@@ -55,7 +55,9 @@ export default function Post({ post }) {
   async function deletePost() {
     if (window.confirm("Are you sure you want to delete this post?")) {
       deleteDoc(doc(db, "posts", post.id));
-      deleteObject(ref(storage, `posts/${post.id}/image`));
+      if (post.data().image) {
+        deleteObject(ref(storage, `posts/${post.id}/image`));
+      }
     }
   }
 
