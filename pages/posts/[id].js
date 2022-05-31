17ef3ba9276a8b4976bc57cd7db86915244e0653
Sahandgha <sahand.gha@gmail.com>
2022-05-31 14:16:09 +1000

    fix deployment errors 2

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -14,7 +14,7 @@ import {
   query,
 } from "firebase/firestore";
 import { db } from "../../firebase";
-import Comment from "../../components/comment";
+import Comment from "../../components/Comment";
 import { AnimatePresence, motion } from "framer-motion";
 
 export default function PostPage({ newsResults, randomUsersResults }) {
